683800e75295eb766f7191e5e29947f7b748578c
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-12 10:28:53 -0500

    feat: npm, dependencies and package.json

---
+++ 13-packages.js
@@ -0,0 +1,27 @@
+// npm - global command, comes with node
+// npm --version
+
+// local dependency - use it only in this particular project
+// npm i <packageName>
+
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo install -g <packageName> (mac)
+//    To install a devDependency, use the -D OR --save-dev flag:
+//    npm install <packageName> -D
+//    npm install <packageName> --save-dev
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+// To uninstall a package
+// npm uninstall <packageName>
+
+const _ = require("lodash");
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems); //[ 1, 2, 3, 4 ]
+
+console.log("hello people");
