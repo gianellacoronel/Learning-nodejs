1d3c03f406642d008d99d1e729e02a3e6e43c54c
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-12 11:51:16 -0500

    feat: creating an event method

--- app.js
+++ app.js
@@ -1,39 +1,11 @@
-const { readFile, writeFile } = require("fs").promises;
-// const util = require("util");
+const EventEmitter = require("events");
 
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(writeFile);
+const customEmitter = new EventEmitter();
+// on - listen for an event
+// emit - emit an event
+customEmitter.on("response", () => {
+  //event named "response"
+  console.log("data received");
+});
 
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, "utf8", (err, result) => {
-//       if (err) {
-//         reject(err);
-//         return;
-//       } else {
-//         resolve(result);
-//       }
-//     });
-//   });
-// };
-
-const start = async () => {
-  try {
-    const first = await readFile("./content/first.txt", "utf8");
-    const second = await readFile("./content/second.txt", "utf8");
-    await writeFile(
-      "./content/result-mind-grenade.txt",
-      `THIS IS AWESOME: ${first} ${second}`,
-      { flag: "a" },
-    );
-    console.log(first, second);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-start();
-
-// getText("./content/first.txt")
-//   .then((result) => console.log(result))
-//   .catch((err) => console.log(err));
+customEmitter.emit("response");
