1d9868e21f3dc9be8e5566b557e0775354d17509
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-16 08:44:19 -0500

    feat: setting express

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,24 +1,12 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+// First, we import the module
+const express = require("express")();
+// Then, we invoke it
+const app = express();
 
-//get all files
-const homePage = readFileSync("");
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>home page</h1>");
-    res.end();
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>about page</h1>");
-    res.end();
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>page not found</h1>");
-    res.end();
-  }
-});
-
-server.listen(5000);
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
