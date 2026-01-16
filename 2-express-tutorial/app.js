3dc82ebdca11f1bb32bddbe23fb4d3e12f242d8c
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-16 08:53:01 -0500

    feat: listening to a port

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,12 +1,16 @@
 // First, we import the module
-const express = require("express")();
+const express = require("express");
 // Then, we invoke it
 const app = express();
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000");
+});
 
 // app.get
 // app.post
 // app.put
 // app.delete
-// app.all
-// app.use
+// app.all -> Works with all of previous methods
+// app.use -> For middleware
 // app.listen
