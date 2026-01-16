97ef7b2f993ad36445e5cf47591217db85e4ce75
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-16 09:42:33 -0500

    feat: send JSON data

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,25 +1,11 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 
-app.use(express.static("./public"));
+const { products } = require("./data");
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-//
-// //adding to static assets
-// //SSR
-// });
-//
-//
-//
-// We add index.html to static assets, and comment app.get,
-// and the page still working as expected.
-
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>Resource not found</h1>");
+app.get("/", (req, res) => {
+  res.json(products);
 });
-
 app.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
