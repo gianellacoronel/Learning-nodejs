8240b265f598538e67cfa6171df22210d6339375
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-16 08:59:40 -0500

    feat: using all to catch routes not defined

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -4,7 +4,17 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Home Page");
+  console.log("User hit the result");
+  res.status(200).send("Home Page");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
+});
+
+//       path,    callback
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Resource not found</h1>");
 });
 
 app.listen(5000, () => {
