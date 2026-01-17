34ae5907f1d2a788cb37437befe94a8e7730d7ad
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-17 10:33:40 -0500

    feat: creating route params

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -17,6 +17,21 @@ app.get("/api/products", (req, res) => {
   res.json(newProducts);
 });
 
+// Route params
+app.get("/api/products/:id", (req, res) => {
+  console.log(req);
+  console.log(req.params); // { id: '1' }
+
+  const { id } = req.params;
+  const singleProduct = products.find((product) => product.id === Number(id));
+  console.log(singleProduct); // if a product does not exist, it will be undefined
+  if (!singleProduct) {
+    return res.status(400).send("Product not found");
+  }
+
+  res.json(singleProduct);
+});
+
 app.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
