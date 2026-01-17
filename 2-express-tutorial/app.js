98bd43e087e25dbb83224220f55137b8ea167aa9
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-17 11:09:58 -0500

    feat: introduction middlewares - getting data in one route

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,70 +1,20 @@
 const express = require("express");
 const app = express();
 
-const { products } = require("./data");
+// req => middleware => res
 
 app.get("/", (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time); // GET / 2026
+  res.send("Home");
 });
 
-app.get("/api/products", (req, res) => {
-  // In some cases, we only want to send some attributes of the product
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-
-  res.json(newProducts);
-});
-
-// Route params
-// http://localhost:5000/api/products/1
-app.get("/api/products/:id", (req, res) => {
-  console.log(req);
-  console.log(req.params); // { id: '1' }
-
-  const { id } = req.params;
-  const singleProduct = products.find((product) => product.id === Number(id));
-  console.log(singleProduct); // if a product does not exist, it will be undefined
-  if (!singleProduct) {
-    return res.status(400).send("Product not found");
-  }
-
-  res.json(singleProduct);
-});
-
-app.get("/api/products/:productId/reviews/:reviewId", (req, res) => {
-  console.log(req.params); //{ productId: '2', reviewId: 'rs' }
-  return res.send("hi params");
-});
-
-// Query string
-// http://localhost:5000/api/v1/query?search=a&limit=2
-app.get("/api/v1/query", (req, res) => {
-  console.log(req.query); // { search: 'a', limit: '2' }
-
-  const { search, limit } = req.query;
-
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-
-  if (sortedProducts.length < 1) {
-    // res.status(200).send("No products matched your search");
-    // It's imporant to put RETURN, to not get an error
-    return res.status(200).json({ success: true, data: [] });
-  }
-  res.status(200).json(sortedProducts);
+app.get("/about", (req, res) => {
+  res.send("About");
 });
 
 app.listen(5000, () => {
-  console.log("Server is listening on port 5000");
+  console.log("Server is running on port 5000");
 });
--- 2-express-tutorial/final/07-params-query.js
+++ 2-express-tutorial/final/07-params-query.js
@@ -1,58 +1,70 @@
-const express = require('express')
-const app = express()
-const { products } = require('./data')
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
-})
-app.get('/api/products', (req, res) => {
+const { products } = require("./data");
+
+app.get("/", (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+});
+
+app.get("/api/products", (req, res) => {
+  // In some cases, we only want to send some attributes of the product
   const newProducts = products.map((product) => {
-    const { id, name, image } = product
-    return { id, name, image }
-  })
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
 
-  res.json(newProducts)
-})
-app.get('/api/products/:productID', (req, res) => {
-  // console.log(req)
-  // console.log(req.params)
-  const { productID } = req.params
+  res.json(newProducts);
+});
 
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  )
+// Route params
+// http://localhost:5000/api/products/1
+app.get("/api/products/:id", (req, res) => {
+  console.log(req);
+  console.log(req.params); // { id: '1' }
+
+  const { id } = req.params;
+  const singleProduct = products.find((product) => product.id === Number(id));
+  console.log(singleProduct); // if a product does not exist, it will be undefined
   if (!singleProduct) {
-    return res.status(404).send('Product Does Not Exist')
+    return res.status(400).send("Product not found");
   }
 
-  return res.json(singleProduct)
-})
+  res.json(singleProduct);
+});
 
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params)
-  res.send('hello world')
-})
+app.get("/api/products/:productId/reviews/:reviewId", (req, res) => {
+  console.log(req.params); //{ productId: '2', reviewId: 'rs' }
+  return res.send("hi params");
+});
 
-app.get('/api/v1/query', (req, res) => {
-  // console.log(req.query)
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
+// Query string
+// http://localhost:5000/api/v1/query?search=a&limit=2
+app.get("/api/v1/query", (req, res) => {
+  console.log(req.query); // { search: 'a', limit: '2' }
+
+  const { search, limit } = req.query;
+
+  let sortedProducts = [...products];
 
   if (search) {
     sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search)
-    })
+      return product.name.startsWith(search);
+    });
   }
+
   if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
+    sortedProducts = sortedProducts.slice(0, Number(limit));
   }
+
   if (sortedProducts.length < 1) {
-    // res.status(200).send('no products matched your search');
-    return res.status(200).json({ sucess: true, data: [] })
+    // res.status(200).send("No products matched your search");
+    // It's imporant to put RETURN, to not get an error
+    return res.status(200).json({ success: true, data: [] });
   }
-  res.status(200).json(sortedProducts)
-})
+  res.status(200).json(sortedProducts);
+});
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log("Server is listening on port 5000");
+});
