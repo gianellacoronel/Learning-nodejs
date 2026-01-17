47d0613d2d3d85912a00967f92ebe12badaa1c63
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-17 11:15:41 -0500

    feat: creating basic middleware function

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -2,16 +2,20 @@ const express = require("express");
 const app = express();
 
 // req => middleware => res
-
-app.get("/", (req, res) => {
+// MIDDLEWARE FUNCTION
+const logger = (req, res, next) => {
   const method = req.method;
   const url = req.url;
   const time = new Date().getFullYear();
   console.log(method, url, time); // GET / 2026
+  next(); // IT'S IMPORTANT TO CALL NEXT() TO MOVE TO THE NEXT MIDDLEWARE FUNCTION
+};
+
+app.get("/", logger, (req, res) => {
   res.send("Home");
 });
 
-app.get("/about", (req, res) => {
+app.get("/about", logger, (req, res) => {
   res.send("About");
 });
 
