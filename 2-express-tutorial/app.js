e9b97de7e8c6727c89250f8017b4bbb11fee3305
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-18 11:54:02 -0500

    feat: DELETE method

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -57,6 +57,21 @@ app.put("/api/people/:id", (req, res) => {
   res.status(200).json({ success: true, data: newPeople });
 });
 
+app.delete("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(400)
+      .json({ success: false, msg: `No person with id ${id}` });
+  }
+
+  const newPeople = people.filter((person) => person.id !== Number(id));
+  return res.status(200).json({ success: true, data: newPeople });
+});
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
