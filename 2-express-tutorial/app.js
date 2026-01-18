c7357aba41862a34526e62b164078c8a3a200b1b
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-18 11:11:13 -0500

    feat: POST method - form example

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -2,8 +2,23 @@ const express = require("express");
 const app = express();
 let { people } = require("./data");
 
+// statice assets
+app.use(express.static("./methods-public"));
+
+// parse form data
+// We have to use here to get body in POST request
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please provide credentials");
 });
 
 app.listen(5000, () => {
